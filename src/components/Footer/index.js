364f4a4b4ea4f8c82b6acb6523101799d93c0631
Rafael <r12rafasantos@gmail.com>
2020-07-29 00:05:25 -0300

    Remover footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,16 +5,9 @@ import Logo from '../../assets/img/logo.png';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
+      <a href="/">
         <img src={Logo} alt="Logo Alura" />
       </a>
-      <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
-      </p>
     </FooterBase>
   );
 }
